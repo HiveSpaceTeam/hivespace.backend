18bc5a5d4017d5ccf498728ee6f1db7954d0bb83
Danh Vu <ledanhvu090888@gmail.com>
2025-05-08 00:32:53 +0700

    Bổ sung Validator Param

--- HiveSpace/HiveSpace.Application/Controllers/OrderController.cs
+++ HiveSpace/HiveSpace.Application/Controllers/OrderController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
 using HiveSpace.Application.Interfaces;
 using HiveSpace.Application.Models.Dtos.Request.Paging;
 using HiveSpace.Application.Models.Dtos.Request.ShoppingCart;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace HiveSpace.Application.Controllers
@@ -13,10 +14,17 @@ namespace HiveSpace.Application.Controllers
     public class OrderController : Controller
     {
         private readonly IOrderService _orderService;
+        private readonly IValidator<CreateOrderRequestDto> _createOrderValidator;
+        private readonly IValidator<PagingRequestDto> _pagingValidator;
 
-        public OrderController(IOrderService orderService)
+        public OrderController(
+            IOrderService orderService,
+            IValidator<CreateOrderRequestDto> createOrderValidator,
+            IValidator<PagingRequestDto> pagingValidator)
         {
             _orderService = orderService;
+            _createOrderValidator = createOrderValidator;
+            _pagingValidator = pagingValidator;
         }
 
         [HttpPost]
@@ -26,10 +34,15 @@ namespace HiveSpace.Application.Controllers
         [ProducesResponseType((int)HttpStatusCode.UnprocessableContent)]
         public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequestDto param)
         {
+            var validationResult = _createOrderValidator.Validate(param);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
             var result = await _orderService.CreateOrder(param);
             return Ok(result);
         }
-
 
         [HttpPost("paging")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
@@ -38,6 +51,12 @@ namespace HiveSpace.Application.Controllers
         [ProducesResponseType((int)HttpStatusCode.UnprocessableContent)]
         public async Task<IActionResult> GetPaging([FromBody] PagingRequestDto param)
         {
+            var validationResult = _pagingValidator.Validate(param);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
             var result = await _orderService.GetPaging(param);
             return Ok(result);
         }
--- HiveSpace/HiveSpace.Application/Controllers/ProductController.cs
+++ HiveSpace/HiveSpace.Application/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
 using HiveSpace.Application.Interfaces;
 using HiveSpace.Application.Models.Dtos.Request.Product;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace HiveSpace.Application.Controllers
@@ -11,10 +12,17 @@ namespace HiveSpace.Application.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
+        private readonly IValidator<ProductSearchRequestDto> _productSearchValidator;
+        private readonly IValidator<ProductHomeRequestDto> _productHomeValidator;
 
-        public ProductController(IProductService productService)
+        public ProductController(
+            IProductService productService,
+            IValidator<ProductSearchRequestDto> productSearchValidator,
+            IValidator<ProductHomeRequestDto> productHomeValidator)
         {
             _productService = productService;
+            _productSearchValidator = productSearchValidator;
+            _productHomeValidator = productHomeValidator;
         }
 
         [HttpGet("{productId}")]
@@ -29,6 +37,12 @@ namespace HiveSpace.Application.Controllers
         [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetProductSearchViewModel([FromBody] ProductSearchRequestDto param)
         {
+            var validationResult = _productSearchValidator.Validate(param);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
             var result = await _productService.GetProductSearchViewModelAsync(param);
             return Ok(result);
         }
@@ -37,6 +51,12 @@ namespace HiveSpace.Application.Controllers
         [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetProductHomeViewModel([FromBody] ProductHomeRequestDto param)
         {
+            var validationResult = _productHomeValidator.Validate(param);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
             var result = await _productService.GetProductHomeViewModelAsync(param);
             return Ok(result);
         }
