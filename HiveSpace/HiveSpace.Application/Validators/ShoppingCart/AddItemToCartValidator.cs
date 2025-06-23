2442b81461a5f05f166a472ffb2e9960495344a7
littlelovevu <69585816+littlelovevu@users.noreply.github.com>
2025-06-23 21:02:18 +0700

    Update validator (#29)
    * bổ sung throw exception
    
    * add unittest
    
    * Update validator

--- HiveSpace/HiveSpace.Application/Validators/ShoppingCart/AddItemToCartValidator.cs
+++ HiveSpace/HiveSpace.Application/Validators/ShoppingCart/AddItemToCartValidator.cs
@@ -1,5 +1,7 @@
 using FluentValidation;
 using HiveSpace.Application.Models.Dtos.Request.ShoppingCart;
+using HiveSpace.Common.Exceptions.Models;
+using HiveSpace.Domain.Enums;
 
 namespace HiveSpace.Application.Validators.ShoppingCart;
 
@@ -8,12 +10,12 @@ public class AddItemToCartValidator : AbstractValidator<AddItemToCartRequestDto>
     public AddItemToCartValidator()
     {
         RuleFor(x => x.SkuId)
-            .NotEmpty().WithMessage("SkuId is required");
+            .NotEmpty().WithState(x => new ErrorCode { Code = ApplicationErrorCode.Required, Source = nameof(x.SkuId) });
 
         RuleFor(x => x.Quantity)
-            .GreaterThan(0).WithMessage("Quantity must be greater than 0");
+            .GreaterThan(0).WithState(x => new ErrorCode { Code = ApplicationErrorCode.InvalidValue, Source = nameof(x.Quantity) });
 
         RuleFor(x => x.IsSelected)
-            .NotEmpty().WithMessage("IsSelected is required");
+            .NotEmpty().WithState(x => new ErrorCode { Code = ApplicationErrorCode.Required, Source = nameof(x.IsSelected) });
     }
 }
